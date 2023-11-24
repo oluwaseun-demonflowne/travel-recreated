1c007104d2520511e78aa8886a38d453d79b534e
demonflowne <ibuemmanuel60@gmail.com>
2023-11-24 18:47:39 +0100

    Changed path in next config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['uploadthing.com','utfs.io'],
         remotePatterns: [
             {
               protocol: 'https',
               hostname: 'uploadthing.com',
               port: '',
-              pathname: '',
+              pathname: '**',
             },
         ],
     }
