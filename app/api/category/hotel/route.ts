303b39290e4db83f2c79922be00669a846ed8aea
demonflowne <ibuemmanuel60@gmail.com>
2023-11-24 18:27:33 +0100

    API must be ready at build time

--- app/api/category/hotel/route.ts
+++ app/api/category/hotel/route.ts
@@ -5,26 +5,26 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 
 
-export const POST = async (req:Request,res:Response) => {
-    const {category,creator,image1,image2,image3,hotelName,shortHotelDescription,location,
-        price,guestNo,roomNo,privateBathNo,cancelFree,reserveNow,specialOffer,
-        breakfast,romantic,airport,overview,wifi,star,hotelType,
-        dedicated,checkIn,freeCancel,wifiIncluded,bathroom,smoking,breakfastIncluded,gym,atm,pool,city,
-      }= await req.json(); 
-    try {    
-        const newFlight = await prisma.hotels.create({
-            data: {category,creator,image1,image2,image3,hotelName,shortHotelDescription,location,
-                price,guestNo,roomNo,privateBathNo,cancelFree,reserveNow,specialOffer,
-                breakfast,romantic,airport,overview,wifi,star,hotelType,
-                dedicated,checkIn,freeCancel,wifiIncluded,bathroom,smoking,breakfastIncluded,gym,atm,pool,city,
-              }
-        })
-        console.log(newFlight)
-        return new Response("done", {status: 200})
-    } catch(error:any) {
-        return new Response(error, {status: 500})
-    }
-}
+// export const POST = async (req:Request,res:Response) => {
+//     const {category,creator,image1,image2,image3,hotelName,shortHotelDescription,location,
+//         price,guestNo,roomNo,privateBathNo,cancelFree,reserveNow,specialOffer,
+//         breakfast,romantic,airport,overview,wifi,star,hotelType,
+//         dedicated,checkIn,freeCancel,wifiIncluded,bathroom,smoking,breakfastIncluded,gym,atm,pool,city,
+//       }= await req.json(); 
+//     try {    
+//         const newFlight = await prisma.hotels.create({
+//             data: {category,creator,image1,image2,image3,hotelName,shortHotelDescription,location,
+//                 price,guestNo,roomNo,privateBathNo,cancelFree,reserveNow,specialOffer,
+//                 breakfast,romantic,airport,overview,wifi,star,hotelType,
+//                 dedicated,checkIn,freeCancel,wifiIncluded,bathroom,smoking,breakfastIncluded,gym,atm,pool,city,
+//               }
+//         })
+//         console.log(newFlight)
+//         return new Response("done", {status: 200})
+//     } catch(error:any) {
+//         return new Response(error, {status: 500})
+//     }
+// }
 
 
 export const GET = async (req:Request,res:Response) => {
