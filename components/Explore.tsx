14a340a9e422886db1882d99bc8ba6cc2255a2af
demonflowne <ibuemmanuel60@gmail.com>
2023-11-27 01:28:31 +0100

    Fixed Scrollbar

--- components/Explore.tsx
+++ components/Explore.tsx
@@ -95,28 +95,28 @@ const Explore = () => {
     }
 
   return (
-    <div className=' my-10 px-6 md:px-0'>
-        <div className='md:flex items-center md:px-28 justify-between'>
+    <div className=' my-10 px-6 md:px-28'>
+        <div className='md:flex items-center  justify-between'>
             <div className='md:flex my-4 md:my-7 flex-col gap-1 text-black'>
                 <h1 className='text-2xl font-black'>Explore to destination</h1>
                 <p className='text-xs'>Lets go on an adventure</p>
             </div>
         </div>
         <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
-        {data?.map((i:Hotels) => (
-            <div key={i.id} className='border relative rounded-2xl overflow-hidden w-56'>
-                <span className='w-9 h-9 bg-white rounded-full opacity-70 flex absolute right-2 top-2 justify-center items-center'><AiOutlineHeart /></span>
-                <Image width={100} height={100} quality={10} src={i.image1} className=' w-56 h-60' alt="images"/>
-                <div className='p-2'>
-                    <Link href={`/hotel/${i.id}`}><h1 className='text-lg font-bold'>{i.hotelName}</h1></Link>
-                    <p className='flex gap-1 text-base items-center'><MdLocationOn className='text-blue-800 text-base' />{i.location}</p>
-                    <div className='flex justify-between mt-6'>
-                        <p className='font-bold text-base'>${i.price}/<span className='text-sm font-light'>per night</span></p>
-                        <p className='flex bg-[rgb(254,247,221)] text-xs font-medium rounded-full p-2 items-center'><AiFillStar className='text-sm text-yellow-400'/>4.9</p>
+            {data?.map((i:Hotels,index:number) => (
+                <div key={i.id} className={`border relative ${index !== 0 ? 'ml-6' : ''} rounded-2xl overflow-hidden w-56`}>
+                    <span className='w-9 h-9 bg-white rounded-full opacity-70 flex absolute right-2 top-2 justify-center items-center'><AiOutlineHeart /></span>
+                    <Image width={100} height={100} quality={10} src={i.image1} className=' w-56 h-60' alt="images"/>
+                    <div className='p-2'>
+                        <Link href={`/hotel/${i.id}`}><h1 className='text-lg font-bold'>{i.hotelName}</h1></Link>
+                        <p className='flex gap-1 text-base items-center'><MdLocationOn className='text-blue-800 text-base' />{i.location}</p>
+                        <div className='flex justify-between mt-6'>
+                            <p className='font-bold text-base'>${i.price}/<span className='text-sm font-light'>per night</span></p>
+                            <p className='flex bg-[rgb(254,247,221)] text-xs font-medium rounded-full p-2 items-center'><AiFillStar className='text-sm text-yellow-400'/>4.9</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-        ))}
+            ))}
         </ScrollMenu>
     </div>
   )
